c74ad1d984ba16b87114a5872286dee186fedc1e
Billy Soup <joeythesoda@gmail.com>
2021-01-14 10:36:03 -0500

    Test

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -3,8 +3,9 @@ import discord
 class MyClient(discord.Client):
 
     async def on_ready(self):
-        
+
         print("Logged on as", self.user)
+        await self.get_channel(713138130374361220).send("!warn @Fake News")
 
 client = MyClient()
 client.run("Nzk4OTY0MjE3NDYxMzQyMjcw.X_8raQ.aGRIb2hNX9iRRFoQPDH7FrUA-h4")
