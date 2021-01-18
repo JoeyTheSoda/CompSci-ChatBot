e44187699177f381775d59e4c05f430fd7feae82
Billy Soup <71106316+JoeyTheSoda@users.noreply.github.com>
2021-01-18 18:43:52 -0500

    Update ChatBot.py

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -6,6 +6,10 @@ class MyClient(discord.Client):
 
         print("Logged on as", self.user)
         await self.get_channel(713138130374361220).send("!warn @Fake News")
+       
+    async def on_message(self, message):
+        
+        await message.add_reaction(":thumbsup:")
 
 client = MyClient()
 client.run("Nzk4OTY0MjE3NDYxMzQyMjcw.X_8raQ.aGRIb2hNX9iRRFoQPDH7FrUA-h4")
